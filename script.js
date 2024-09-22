361a4ce0ad86b13c9aac331a27681ebf290b5d8e
Ibnu1102 <nuuyz11@gmail.com>
2024-09-22 17:12:27 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+// Handling slide menu
+const slideMenu = document.getElementById('slide-menu');
+const closeButton = document.querySelector('.close-button');
+const slideMenuForm = document.getElementById('slide-menu-form');
+const orderButtons = document.querySelectorAll('.order-button');
+
+orderButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        slideMenu.style.display = 'block';
+    });
+});
+
+closeButton.addEventListener('click', () => {
+    slideMenu.style.display = 'none';
+});
+
+window.addEventListener('click', event => {
+    if (event.target === slideMenu) {
+        slideMenu.style.display = 'none';
+    }
+});
+
+slideMenuForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const item = document.getElementById('slide-item').value;
+    const theme = document.getElementById('slide-theme').value;
+
+    const message = `Item atau produk: ${item}\nTema atau model: ${theme}`;
+    const whatsappUrl = `https://wa.me/6282220325507?text=${encodeURIComponent(message)}`;
+
+    window.location.href = whatsappUrl;
+});
